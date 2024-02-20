e40d2b1debf11ce706bd22a67467a5b25a0b7304
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-02-20 16:19:15 +0100

    Bash script to launch the experiments

---
+++ experiments/launch_exp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+printf "Executing DIFFI Experiments:\n"
+
+# Path to the Python script to execute 
+SCRIPT_PATH="test_DIFFI_importances.py"
+
+#DATASETS="wine glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
+DATASETS="wine"
+
+DATASET_PATHS="../data/real/"
+
+python $SCRIPT_PATH \
+    --dataset_name $DATASETS \
+    --n_runs 10 \
+    --split True \
+    --n_estimators 300 
+
+
+# Possible parameters to add (for now they are at the default value)
+# --dataset_path (if different from ../data/real/)
+# --max_samples 10 \
+# --train_size 0.7 \
+# --contamination 0.05 \ 
