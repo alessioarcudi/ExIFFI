f40495d4404bc7ce5a8f6aab1dbec86e90229939
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-02-20 16:19:01 +0100

    Fix experiment script

--- experiments/test_DIFFI_importances.py
+++ experiments/test_DIFFI_importances.py
@@ -1,6 +1,6 @@
 import sys
 import os
-os.chdir('/home/davidefrizzo/PHD/ExIFFI/experiments')
+#os.chdir('/home/davidefrizzo/PHD/ExIFFI/experiments')
 #os.chdir('/Users/alessio/Documents/ExIFFI/experiments')
 sys.path.append("..")
 from collections import namedtuple
@@ -39,18 +39,19 @@ n_runs = args.n_runs
 # Load the dataset
 dataset = Dataset(dataset_name, path = dataset_path)
 dataset.drop_duplicates()
+# Create the contaminated trainig set 
+dataset.split_dataset(train_size=args.train_size,contamination=args.contamination)
 
 # Normalize the data
 if args.split:
-    dataset.split_dataset(train_size=args.train_size,contamination=args.contamination)
-    dataset.normalize(split=True)
+    dataset.pre_process()
 else:
-    dataset.normalize(split=False)
+    dataset.pre_process(split=False)
 
 # Initalize the Isolation Forest model 
 I=IsolationForest(n_estimators=n_estimators,max_samples=max_samples,contamination=args.contamination)
 
-cwd='/home/davidefrizzo/PHD/ExIFFI'
+cwd='/home/davidefrizzo/Desktop/PHD/ExIFFI'
 #cwd = '/Users/alessio/Documents/ExIFFI'
 
 path = cwd +"/experiments/results/"+dataset.name
@@ -65,36 +66,42 @@ if not os.path.exists(path_plots):
 
 #----------------- GLOBAL IMPORTANCES -----------------#
 # initialize global_importances paths
-path_experiment = path_experiments + "/global_importances"
-if not os.path.exists(path_experiment):
-    os.makedirs(path_experiment)
-path_experiment = path_experiments + "/DIFFI"
-if not os.path.exists(path_experiment):
-    os.makedirs(path_experiment)
-path_experiment_matrices = path_experiment + "/matrices"
-if not os.path.exists(path_experiment_matrices):
-    os.makedirs(path_experiment_matrices)
-path_experiment_plots = path_experiment + "/data_for_plots"
-if not os.path.exists(path_experiment_plots):
-    os.makedirs(path_experiment_plots)
+path_experiments = path_experiments + "/global_importances"
+if not os.path.exists(path_experiments):
+    os.makedirs(path_experiments)
+path_experiments = path_experiments + "/DIFFI"
+if not os.path.exists(path_experiments):
+    os.makedirs(path_experiments)
+
+# path_experiment_matrices = path_experiment + "/matrices"
+# if not os.path.exists(path_experiment_matrices):
+#     os.makedirs(path_experiment_matrices)
+# path_experiment_plots = path_experiment + "/data_for_plots"
+# if not os.path.exists(path_experiment_plots):
+#     os.makedirs(path_experiment_plots)
 
 #Compute global importances
-full_importances, data_for_plots = compute_global_importances(I, dataset,n_runs,p=contamination,isdiffi=True)    
-save_element(full_importances, path_experiment_matrices, filetype="npz")
-save_element(data_for_plots, path_experiment_plots, filetype="pickle")
+full_importances = compute_global_importances(I, dataset,isdiffi=True,n_runs=n_runs,p=contamination)    
+save_element(full_importances, path_experiments, filename=f'{dataset.name}_imp_mat', filetype="npz")
 
 # plot global importances
-most_recent_file = get_most_recent_file(path_experiment_matrices)
-bar_plot(dataset, most_recent_file, filetype="npz", plot_path=path_plots, show_plot=False)
-most_recent_file = get_most_recent_file(path_experiment_plots)
-score_plot(dataset, most_recent_file, plot_path=path_plots, show_plot=False)
+most_recent_file = get_most_recent_file(path_experiments)
+bar_plot(dataset, most_recent_file, filetype="npz", plot_path=path_plots,show_plot=False,model_name="DIFFI")
+print("Bar Plot produced and saved in: ", path_plots)
+
+
+score_plot(dataset, most_recent_file, plot_path=path_plots,show_plot=False,model_name="DIFFI")
+print("Score Plot produced and saved in: ", path_plots)
 
 #----------------- LOCAL SCOREMAP -----------------#
 # initialize local_scoremap paths
-path_experiment = path_experiments + "/local_scoremap"
-if not os.path.exists(path_experiment):
-    os.makedirs(path_experiment)
+# path_experiment = path_experiments + "/local_scoremap"
+# if not os.path.exists(path_experiments):
+#     os.makedirs(path_experiments)
 
 # Compute local scoremap
+print('Fit the model for the importance map')
 I.fit(dataset.X)
-importance_map(dataset,I,feats_plot=(12,4),path_plot=path_plots,isdiffi=True,iforest=I)
+print('Start computing Importance Map')
+importance_map(dataset,I,feats_plot=(12,4),path_plot=path_plots,isdiffi=True,iforest=I,show_plot=False)
+print('Importance Map produced and saved in: ', path_plots)
