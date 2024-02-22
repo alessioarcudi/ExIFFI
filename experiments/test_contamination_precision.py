6283f92e649253aeba70c3595372d962d9d7f4a7
AlesArcudi <alessio.arcudi@studenti.unipd.it.com>
2024-02-22 18:18:22 +0100

    update contamination experiments

--- experiments/test_contamination_precision.py
+++ experiments/test_contamination_precision.py
@@ -10,6 +10,8 @@ from utils_reboot.datasets import *
 from utils_reboot.plots import *
 from utils_reboot.utils import *
 from pyod.models.dif import DIF
+from pyod.models.auto_encoder import AutoEncoder
+from sklearn.ensemble import IsolationForest as sklearn_IsolationForest
 
 
 from model_reboot.EIF_reboot import ExtendedIsolationForest
@@ -21,16 +23,17 @@ parser = argparse.ArgumentParser(description='Test Global Importances')
 # Add the arguments
 parser.add_argument('--dataset_name', type=str, default='wine', help='Name of the dataset')
 parser.add_argument('--dataset_path', type=str, default='../data/real/', help='Path to the dataset')
-parser.add_argument('--model_name', type=str, default="EIF+", help='Name of the model')
-parser.add_argument('--interpretation_algorithm', type=str, default="EXIFFI", help='Name of the interpretation algorithm')
+
 parser.add_argument('--n_estimators', type=int, default=200, help='EIF parameter: n_estimators')
 parser.add_argument('--max_depth', type=str, default='auto', help='EIF parameter: max_depth')
 parser.add_argument('--max_samples', type=str, default='auto', help='EIF parameter: max_samples')
 parser.add_argument('--contamination', type=npt.NDArray, default=np.linspace(0.0,0.1,11), help='Global feature importances parameter: contamination')
 parser.add_argument('--n_runs', type=int, default=10, help='Global feature importances parameter: n_runs')
 parser.add_argument('--train_size', type=float, default=0.9, help='Global feature importances parameter: train_size')
-parser.add_argument('--compute_global_importances', type=bool, default=True, help='Global feature importances parameter: compute_global_importances')
+parser.add_argument('--compute_global_importances', type=bool, default=False, help='Global feature importances parameter: compute_global_importances')
 
+parser.add_argument('--model', type=str, default="DIF", help='Name of the model')
+parser.add_argument('--interpretation', type=str, default="EXIFFI", help='Name of the interpretation algorithm')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -38,31 +41,44 @@ args = parser.parse_args()
 # Access the arguments
 dataset_name = args.dataset_name
 dataset_path = args.dataset_path
-model_name = args.model_name
+
 n_estimators = args.n_estimators
 max_depth = args.max_depth
 max_samples = args.max_samples
 contamination = args.contamination
 n_runs = args.n_runs
 train_size = args.train_size
-compute_global_importances = args.compute_global_importances
-isdiffi = model_name == "DIFFI"
-if model_name not in ["EIF+","EIF","DIFFI"] and compute_global_importances:
-    compute_global_importances = False
-    print("impossible to compute importances for model ", model_name)
+
+GFI = args.compute_global_importances
+model = args.model
+interpretation = args.interpretation
+
+
+assert model in ["IF", "EIF", "EIF+", "DIF", "AnomalyAutoencoder"], "Model not recognized"
+assert interpretation in ["EXIFFI", "DIFFI", "RandomForest"], "Interpretation not recognized"
+
+if model == "DIF" and GFI:
+    raise ValueError("DIF model does not support global feature importances")
+if model == "AnomalyAutoencoder" and GFI:
+    raise ValueError("AnomalyAutoencoder model does not support global feature importances")
+
 
 
 dataset = Dataset(dataset_name, path = dataset_path)
 dataset.drop_duplicates()
 
-if model_name == "EIF+":
+if model == "EIF+":
     I = ExtendedIsolationForest(1, n_estimators=n_estimators, max_depth=max_depth, max_samples=max_samples)
-elif model_name == "EIF":
+elif model == "EIF":
     I = ExtendedIsolationForest(0, n_estimators=n_estimators, max_depth=max_depth, max_samples=max_samples)
-elif model_name == "IF":
-    I = IsolationForest(n_estimators=n_estimators, max_samples=max_samples)
-elif model_name == "DIF":
+elif (model == "IF" and interpretation == "EXIFFI") or (model =="IF" and not GFI):
+    I = IsolationForest(n_estimators=n_estimators, max_depth=max_depth, max_samples=max_samples)
+elif model == "IF" and interpretation == "DIFFI":
+    I = sklearn_IsolationForest(n_estimators=n_estimators, max_samples=max_samples)
+elif model == "DIF":
     I = DIF(n_estimators=n_estimators, max_samples=max_samples)
+elif model == "AnomalyAutoencoder":
+    I = AutoEncoder(hidden_neurons=[dataset.X.shape[1], 32, 32, dataset.X.shape[1]], contamination=0.1, epochs=100, random_state=42,verbose =0)
     
 
 cwd = '/Users/alessio/Documents/ExIFFI'
@@ -79,37 +95,34 @@ if not os.path.exists(path_plots):
 
 #----------------- EVALUATE PRECISIONS OVER CONTAMINATION -----------------#
 # initialize contamiination paths
-path_experiment = path_experiments + "/contamination"
-if not os.path.exists(path_experiment):
-    os.makedirs(path_experiment)
-path_experiment_matrices = path_experiment + "/precisions"
-if not os.path.exists(path_experiment_matrices):
-    os.makedirs(path_experiment_matrices)
-if isdiffi:
-    path_importances = path_experiments + "/global_importances/DIFFI"
-    if not os.path.exists(path_importances):
-        os.makedirs(path_importances)
-    path_importances_over_contamination = path_importances + "/contamination"
-    if not os.path.exists(path_importances_over_contamination):
-        os.makedirs(path_importances_over_contamination)
-else:
-    path_importances = path_experiments + "/global_importances/EXIFFI"
-    if not os.path.exists(path_importances):
-        os.makedirs(path_importances)
-    path_importances_over_contamination = path_importances + "/contamination"
-    if not os.path.exists(path_importances_over_contamination):
-        os.makedirs(path_importances_over_contamination)
+path_experiment_contamination = path_experiments + "/contamination"
+if not os.path.exists(path_experiment_contamination):
+    os.makedirs(path_experiment_contamination)
+path_experiment_global_importances = path_experiments + "/global_importances/contamination"
+if not os.path.exists(path_experiment_global_importances):
+    os.makedirs(path_experiment_global_importances)
+
+path_experiment_contamination_model = path_experiment_contamination + "/" + model
+if not os.path.exists(path_experiment_contamination_model):
+    os.makedirs(path_experiment_contamination_model)
+
+path_experiment_global_importances_model = path_experiment_global_importances + "/" + model
+if not os.path.exists(path_experiment_global_importances_model):
+    os.makedirs(path_experiment_global_importances_model)
+path_experiment_global_importances_model_interpretation = path_experiment_global_importances_model + "/" + interpretation
+if not os.path.exists(path_experiment_global_importances_model_interpretation):
+    os.makedirs(path_experiment_global_importances_model_interpretation)
 
 # contamination evaluation
-if compute_global_importances:
-    precisions, importances = contamination_in_training_precision_evaluation(I, dataset, n_runs, train_size=train_size, contamination_values=contamination, compute_global_importances=compute_global_importances, isdiffi=isdiffi)
-    save_element((importances,contamination), path_importances_over_contamination, filetype="pickle")
-    save_element((precisions,contamination), path_experiment_matrices, filetype="pickle")
+if GFI:
+    precisions, importances = contamination_in_training_precision_evaluation(I, dataset, n_runs, train_size=train_size, contamination_values=contamination, compute_global_importances=GFI, interpretation = interpretation)
+    save_element((importances,contamination), path_experiment_global_importances_model_interpretation, filetype="pickle")
+    save_element((precisions,contamination), path_experiment_contamination_model, filetype="pickle")
 else:
-    precisions = contamination_in_training_precision_evaluation(I, dataset, n_runs, train_size=train_size, contamination_values=contamination, compute_global_importances=compute_global_importances, isdiffi=isdiffi)
-    save_element((precisions,contamination), path_experiment_matrices, filetype="pickle")
+    precisions = contamination_in_training_precision_evaluation(I, dataset, n_runs, train_size=train_size, contamination_values=contamination, compute_global_importances=GFI, interpretation = interpretation)
+    save_element((precisions,contamination), path_experiment_contamination_model, filetype="pickle")
 
 #plot contamination evaluation
-(precisions,contamination) = open_element(get_most_recent_file(path_experiment_matrices))
+(precisions,contamination) = open_element(get_most_recent_file(path_experiment_contamination_model))
 plot_precision_over_contamination(precisions,  I.name, path_plots, contamination=contamination, plot_image=False)
 
