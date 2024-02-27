c670b594583fb37c5efa9e7327b8748009fb02f3
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-02-27 18:39:28 +0100

    tests

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -3,17 +3,18 @@
 # Path to the Python script to execute
 # SCRIPT_PATH="test_global_importancies.py"
 # SCRIPT_PATH="test_feature_selection.py"
-SCRIPT_PATH="test_GFI_FS.py"
+SCRIPT_PATH="test_contamination_precision.py"
+#SCRIPT_PATH="test_GFI_FS.py"
 
 # List of datasets
-#DATASETS="wine"
-DATASETS="bisect bisect_3d bisect_6d"
+#DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
+DATASETS="wine"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
 
 # Path to the datasets
-DATASET_PATH="../data/syn/"
+DATASET_PATH="../data/real/"
 
 # Iterate over the datasets and call the Python command for each dataset
 for dataset in "${DATASET_ARRAY[@]}"; do
@@ -21,8 +22,8 @@ for dataset in "${DATASET_ARRAY[@]}"; do
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
         --pre_process \
-        --model "EIF" \
-        --interpretation "RandomForest" \
+        --model "EIF+" \
+        --interpretation "EXIFFI" \
         --scenario 2 \
 
 done
