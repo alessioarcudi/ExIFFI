befd2dc078edc691eeef52d6c656f5e8f2e38876
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-02-29 17:05:06 +0100

    Experiment results

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -3,19 +3,20 @@
 # Path to the Python script to execute
 # SCRIPT_PATH="test_global_importancies.py"
 #SCRIPT_PATH="test_feature_selection.py"
-SCRIPT_PATH="test_contamination_precision.py"
-#SCRIPT_PATH="test_GFI_FS.py"
+#SCRIPT_PATH="test_contamination_precision.py"
+SCRIPT_PATH="test_GFI_FS.py"
 
 # List of datasets
 #DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
-#DATASETS="wine glass cardio pima breastw ionosphere annthyroid pendigits"
-DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
+DATASETS="wine glass cardio pima breastw ionosphere annthyroid pendigits"
+#DATASETS="Yaxis bisect bisect_3d bisect_6d"
+#DATASETS="wine"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
 
 # Path to the datasetsok, put the `xlim` at 1.1
-DATASET_PATH="../data/syn/"
+DATASET_PATH="../data/real/"
 
 # Iterate over the datasets and call the Python command for each dataset
 for dataset in "${DATASET_ARRAY[@]}"; do
@@ -24,7 +25,7 @@ for dataset in "${DATASET_ARRAY[@]}"; do
         --dataset_path "$DATASET_PATH" \
         --pre_process \
         --model "IF" \
-        --interpretation "DIFFI" \
-        --scenario 1
+        --interpretation "RandomForest" \
+        --scenario 2
 
 done
