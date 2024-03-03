27300c4c9c92429739b6e3b228d0778e0a2a2e4b
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-03-03 08:27:55 +0100

    Experiment Results

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -10,7 +10,7 @@ SCRIPT_PATH="test_GFI_FS.py"
 DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
 #DATASETS="wine glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
 #DATASETS="Yaxis bisect bisect_3d bisect_6d"
-#DATASETS="Yaxis"
+#DATASETS="ionosphere"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
@@ -24,8 +24,8 @@ for dataset in "${DATASET_ARRAY[@]}"; do
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
         --pre_process \
-        --model "EIF" \
-        --interpretation "EXIFFI" \
+        --model "EIF+" \
+        --interpretation "RandomForest" \
         --scenario 1 \
         --include_random \
         --downsample \
