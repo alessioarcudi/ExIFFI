9dc14ef5ad65a7e858710ad073823eb1fecec854
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-03-06 20:29:40 +0100

    Experiment Results

--- experiments/launch_exp_GFI.sh
+++ experiments/launch_exp_GFI.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Path to the Python script to execute
-# SCRIPT_PATH="test_global_importancies.py"
-SCRIPT_PATH="test_feature_selection.py"
-#SCRIPT_PATH="test_GFI_FS.py"
+SCRIPT_PATH="test_global_importancies.py"
 
 # List of datasets
 #DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
 #DATASETS="glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
 #DATASETS="Yaxis bisect bisect_3d bisect_6d"
-DATASETS="annthyroid"
+DATASETS="glass_DIFFI"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
@@ -23,10 +21,9 @@ for dataset in "${DATASET_ARRAY[@]}"; do
     python $SCRIPT_PATH \
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
-        --model "EIF+" \
-        --model_interpretation "EIF+" \
-        --interpretation "EXIFFI+" \
+        --model "IF" \
+        --interpretation "RandomForest" \
         --pre_process \
-        --scenario 2
+        --scenario 1
 
 done
