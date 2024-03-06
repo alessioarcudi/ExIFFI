b36fef6cece1edb2e00170586e82dc751d3376ef
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-03-06 10:18:38 +0100

    Experiment Results (almost all Feature Selection Plots of annthyroid)

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -9,7 +9,7 @@ SCRIPT_PATH="test_feature_selection.py"
 #DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
 #DATASETS="glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
 #DATASETS="Yaxis bisect bisect_3d bisect_6d"
-DATASETS="glass"
+DATASETS="annthyroid"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
@@ -24,7 +24,7 @@ for dataset in "${DATASET_ARRAY[@]}"; do
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
         --model "EIF" \
-        --model_interpretation "IF" \
+        --model_interpretation "EIF" \
         --interpretation "RandomForest" \
         --pre_process \
         --scenario 1
