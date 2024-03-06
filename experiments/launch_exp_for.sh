ea933ce7a4e6dcc10c17e19bd131024c7836dd9f
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-03-06 20:56:47 +0100

    Experiment Results

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -9,7 +9,7 @@ SCRIPT_PATH="test_feature_selection.py"
 #DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
 #DATASETS="glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
 #DATASETS="Yaxis bisect bisect_3d bisect_6d"
-DATASETS="annthyroid"
+DATASETS="glass_DIFFI"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
@@ -23,9 +23,9 @@ for dataset in "${DATASET_ARRAY[@]}"; do
     python $SCRIPT_PATH \
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
-        --model "EIF+" \
-        --model_interpretation "EIF+" \
-        --interpretation "EXIFFI+" \
+        --model "EIF" \
+        --model_interpretation "IF" \
+        --interpretation "RandomForest" \
         --pre_process \
         --scenario 2
 
