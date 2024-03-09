554c9cc714a10acb2d1e3676f3e696ceac5dfc29
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-03-09 18:22:48 +0100

    Experiment Results

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the Python script to execute
-# SCRIPT_PATH="test_global_importancies.py"
+#SCRIPT_PATH="test_global_importancies.py"
 SCRIPT_PATH="test_feature_selection.py"
 #SCRIPT_PATH="test_GFI_FS.py"
 #SCRIPT_PATH="test_local_importances.py"
@@ -10,13 +10,13 @@ SCRIPT_PATH="test_feature_selection.py"
 #DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
 #DATASETS="glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
 #DATASETS="Yaxis bisect bisect_3d bisect_6d"
-DATASETS="glass_DIFFI"
+DATASETS="bisect"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
 
 # Path to the datasets 
-DATASET_PATH="../data/real/"
+DATASET_PATH="../data/syn/"
 
 # Iterate over the datasets and call the Python command for each dataset
 for dataset in "${DATASET_ARRAY[@]}"; do
@@ -24,12 +24,13 @@ for dataset in "${DATASET_ARRAY[@]}"; do
     python $SCRIPT_PATH \
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
-        --model "IF" \
-        --model_interpretation "EIF+" \
-        --interpretation "EXIFFI+" \
+        --model "EIF" \
+        --model_interpretation "IF" \
+        --interpretation "RandomForest" \
         --pre_process \
-        --scenario 2 \
+        --scenario 1  
 
 done
 
 #--feats_plot "(10,0)" \
+#
