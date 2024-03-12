5c5fa76a2ac09635aa146edc5322537e96cff31f
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-03-12 09:46:07 +0100

    Redo Importance and Feature Selection plots for Xaxis without pre processing the data

--- experiments/launch_exp_for.sh
+++ experiments/launch_exp_for.sh
@@ -2,8 +2,8 @@
 
 # Path to the Python script to execute
 #SCRIPT_PATH="test_global_importancies.py"
-#SCRIPT_PATH="test_feature_selection.py"
-SCRIPT_PATH="test_metrics.py"
+SCRIPT_PATH="test_feature_selection.py"
+#SCRIPT_PATH="test_metrics.py"
 #SCRIPT_PATH="test_GFI_FS.py"
 #SCRIPT_PATH="test_local_importances.py"
 
@@ -11,13 +11,13 @@ SCRIPT_PATH="test_metrics.py"
 #DATASETS="Xaxis Yaxis bisect bisect_3d bisect_6d"
 #DATASETS="glass cardio pima breastw ionosphere annthyroid pendigits diabetes shuttle moodify"
 #DATASETS="Yaxis bisect bisect_3d bisect_6d"
-DATASETS="moodify"
+DATASETS="Xaxis"
 
 # Split the DATASETS string into an array
 IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
 
 # Path to the datasets 
-DATASET_PATH="../data/real/"
+DATASET_PATH="../data/syn/"
 
 # Iterate over the datasets and call the Python command for each dataset
 for dataset in "${DATASET_ARRAY[@]}"; do
@@ -25,8 +25,9 @@ for dataset in "${DATASET_ARRAY[@]}"; do
     python $SCRIPT_PATH \
         --dataset_name "$dataset" \
         --dataset_path "$DATASET_PATH" \
-        --model "AnomalyAutoencoder" \
-        --pre_process 1 \
+        --model "EIF" \
+        --model_interpretation "IF" \
+        --interpretation "RandomForest" \
         --scenario 1
 
 done
