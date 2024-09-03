b1c6f8746d0cbf2ef32030d658f2b4fbcd982c5d
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2024-09-03 20:43:18 +0200

    Correlation exp synthetic datasets

--- experiments/launch_correlation.sh
+++ experiments/launch_correlation.sh
@@ -2,42 +2,47 @@
 
 SCRIPT_PATH="test_correlation.py"
 
-DATASETS="moodify"
+DATASETS="Xaxis bisect bisect_3d bisect_3d_prop bisect_6d"
+
+IFS=' ' read -ra DATASET_ARRAY <<< "$DATASETS"
 
 # Next datasets → shuttle moodify
 
-DATASET_PATH="../data/real/"
+DATASET_PATH="../data/syn/"
 
-python $SCRIPT_PATH \
-        --dataset_name $DATASETS \
-        --dataset_path $DATASET_PATH \
-        --model "EIF+" \
-        --interpretation "EXIFFI+" \
-        --scenario 2 
+for dataset in "${DATASET_ARRAY[@]}"; do
 
-python $SCRIPT_PATH \
-        --dataset_name $DATASETS \
-        --dataset_path $DATASET_PATH \
-        --model "EIF" \
-        --interpretation "EXIFFI" \
-        --scenario 2 
+        python $SCRIPT_PATH \
+                --dataset_name "$dataset" \
+                --dataset_path $DATASET_PATH \
+                --model "EIF+" \
+                --interpretation "EXIFFI+" \
+                --scenario 1 
 
-python $SCRIPT_PATH \
-        --dataset_name $DATASETS \
-        --dataset_path $DATASET_PATH \
-        --model "IF" \
-        --interpretation "EXIFFI" \
-        --scenario 2 
+        python $SCRIPT_PATH \
+                --dataset_name "$dataset" \
+                --dataset_path $DATASET_PATH \
+                --model "EIF" \
+                --interpretation "EXIFFI" \
+                --scenario 1 
 
-python $SCRIPT_PATH \
-        --dataset_name $DATASETS \
-        --dataset_path $DATASET_PATH \
-        --model "IF" \
-        --interpretation "DIFFI" \
-        --scenario 2 
+        python $SCRIPT_PATH \
+                --dataset_name "$dataset" \
+                --dataset_path $DATASET_PATH \
+                --model "IF" \
+                --interpretation "EXIFFI" \
+                --scenario 1 
+
+        python $SCRIPT_PATH \
+                --dataset_name "$dataset" \
+                --dataset_path $DATASET_PATH \
+                --model "IF" \
+                --interpretation "DIFFI" \
+                --scenario 1 
+done
 
 # python $SCRIPT_PATH \
-#         --dataset_name $DATASETS \
+#         --dataset_name $dataset \
 #         --dataset_path $DATASET_PATH \
 #         --model "EIF+" \
 #         --interpretation "EIF+_RandomForest" \
